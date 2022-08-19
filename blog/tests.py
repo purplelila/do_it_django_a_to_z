6ca07dfaff4416178c3a714062adf1e2017ea4e0
purplelila <wjdgus6226@donga.ac.kr>
2022-08-19 11:16:08 +0900

    검색 기능 구현하기

--- blog/tests.py
+++ blog/tests.py
@@ -277,3 +277,22 @@ class TestView(TestCase):
         self.assertIn('한글 태그', main_area.text)
         self.assertIn('some tag', main_area.text)
         self.assertNotIn('python', main_area.text)
+
+    def test_search(self):
+        post_about_python = Post.objects.create(
+            title='파이썬 포스트임돠.',
+            content='기다렸어 어서와 Hello future!',
+            author=self.user_trump
+        )
+
+        response = self.client.get('/blog/search/파이썬/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        main_area = soup.find('div', id='main-area')
+
+        self.assertIn('Search: 파이썬 (2)', main_area.text)
+        self.assertNotIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertIn(self.post_003.title, main_area.text)
+        self.assertIn(post_about_python.title, main_area.text)
